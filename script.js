6f8497cd9d087cfa31266ba00fd2564fceda0aa1
Abhishek Shah <abhishek.code@outlook.com>
2023-04-12 22:11:55 +0530

    added starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector("btn--new");
+const btnRoll = document.querySelector("btn--roll");
+const btnhold = document.querySelector("btn--hold");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
